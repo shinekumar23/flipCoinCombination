a270af57630548ab596000d4a8acaa592260cb3d
shinekumar23 <shinekumar23@gmail.com>
2020-08-06 13:46:44 +0530

    Added doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -21,3 +21,31 @@ y=$(echo "scale=2;$numberOfSingletTails/7*100" | bc)
 Tails_Singlet_Percentage=$(echo ${y%.*})
 echo "Heads Singlet percentage is $Heads_Singlet_Percentage"
 echo "Tails Singlet percentage is $Tails_Singlet_Percentage"
+
+declare -A doublet
+for (( i=1; i<=7; i++ ))
+do
+         flip=$((RANDOM%4))
+         case $flip in
+                   0) face=HT
+                      ;;
+                   1) face=HH
+                      ;;
+                   2) face=TH
+                      ;;
+                   3) face=TT
+                      ;;
+         esac
+         doublet[$flip]=$face
+         doubletArray[k++]=$face
+done
+echo "The doublet combination: ${doubletArray[@]}"
+numberOfDoubletTails=$(echo ${doubletArray[@]} | tr -cd 'T' | wc -c)
+numberOfDoubletHeads=$(echo ${doubletArray[@]} | tr -cd 'H' | wc -c)
+z=$(echo "scale=2;$numberOfDoubletHeads/14*100" | bc)
+Heads_Doublet_Percentage=$(echo ${z%.*})
+a=$(echo "scale=2;$numberOfDoubletTails/14*100" | bc)
+Tails_Doublet_Percentage=$(echo ${a%.*})
+echo "The doublet Heads Percentage is $Heads_Doublet_Percentage"
+echo "The doublet Tails Percentage is $Tails_Doublet_Percentage"
+
