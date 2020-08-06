b014e63410e36f4b9c892f044013bad1aabd0f21
shinekumar23 <shinekumar23@gmail.com>
2020-08-06 13:39:57 +0530

    Added singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -x
-flip=$((RANDOM%2))
-case $flip in
-			0) echo "Heads"
-				;;
-			1) echo "Tails"
-				;;
-esac
+declare -A singlet
+for (( i=1; i<=7; i++ ))
+do
+            flip=$((RANDOM%2))
+            case $flip in
+                     0) face=H
+                        ;;
+                     1) face=T
+                        ;;
+            esac
+				singlet[$flip]="$face"
+				singletArray[j++]=$face
+done
+echo "The singlet combination: ${singletArray[@]}"
+numberOfSingletHeads=$(echo ${singletArray[@]} | tr -cd 'H' | wc -c)
+numberOfSingletTails=$(echo ${singletArray[@]} | tr -cd 'T' | wc -c)
+x=$(echo "scale=2;$numberOfSingletHeads/7*100" | bc)
+Heads_Singlet_Percentage=$(echo ${x%.*})
+y=$(echo "scale=2;$numberOfSingletTails/7*100" | bc)
+Tails_Singlet_Percentage=$(echo ${y%.*})
+echo "Heads Singlet percentage is $Heads_Singlet_Percentage"
+echo "Tails Singlet percentage is $Tails_Singlet_Percentage"
